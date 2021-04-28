77e3041089f722df5114dada4c2a53270a1c298d
Bartosz <weivan321@gmail.com>
2021-04-28 21:37:19 +0200

    going pretty well added first animations JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
-let controller = new ScrollMagic.controller
+let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
+
+timeline.to('.hand', 3, { y: -150 })
+    .to('.forest', 3, { y: 50 }, '-=3')
+    .to('.heading__text', 3, { x: 50 }, '-=3');
+
+let scene = new ScrollMagic.Scene({
+    triggerElement: 'section',
+    duration: '100%',
+    triggerHook: 0.5,
+})
+    .setTween(timeline)
+    .addTo(controller)
