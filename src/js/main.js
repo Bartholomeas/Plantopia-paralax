af07493ce52f4689b921bce7795e1804ec57c00d
Bartosz <weivan321@gmail.com>
2021-05-03 16:11:48 +0200

    two sections finished

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,13 @@
 let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
 
-timeline.to('.hand', 3, { y: 100 })
+timeline.to('.hand', 3, { y: -350 })
     .to('.forest', 3, { y: -50 }, '-=3')
     .to('.heading__text', 3, { y: 50 }, '-=3')
-    .to('.goal__section', 3, { top: '0%' }, '-=3');
-// .fromTo('.goal__item-circles', 3, { opacity: 0 }, { opacity: 1, duration: 3 }, '-=3');
+    .to('.goal__section', 3, { top: '0%' }, '-=3')
+    .fromTo('.goal__item-circles-top', 3, { x: -150 }, { x: 0 }, '-=3')
+    .fromTo('.goal__item-circles-bottom', 3, { x: 150 }, { x: 0 }, '-=3');
+// .to('.goal__item-circles', 3, { opacity: 1, duration: 3 }, '-=3');
 // .to('.goal__section', 3, { top: "0%" }, '-=3')
 
 
@@ -15,5 +17,5 @@ let scene = new ScrollMagic.Scene({
     triggerHook: 0,
 })
     .setTween(timeline)
-    .setPin('section')
+    .setPin('.goal__section')
     .addTo(controller)
