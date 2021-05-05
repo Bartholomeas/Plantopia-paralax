945bc230c8c2763a26ed5fda4d9027ac9f4527c2
Bartosz <weivan321@gmail.com>
2021-05-05 14:42:40 +0200

    finished, paralax works good

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
 
-timeline.to('.hand', 3, { y: -350 })
-    .to('.forest', 3, { y: -50 }, '-=3')
-    .to('.heading__text', 3, { y: 50 }, '-=3')
+timeline.to('.hand', 3, { y: 50 })
+    .to('.forest', 3, { y: -250 }, '-=3')
+    .to('.heading__text', 3, { y: -450 }, '-=3')
     .to('.goal__section', 3, { top: '0%' }, '-=3')
     .fromTo('.goal__item-circles-top', 3, { x: -150 }, { x: 0 }, '-=3')
     .fromTo('.goal__item-circles-bottom', 3, { x: 150 }, { x: 0 }, '-=3');
@@ -11,10 +11,10 @@ timeline.to('.hand', 3, { y: -350 })
 
 
 let scene = new ScrollMagic.Scene({
-    triggerElement: "header",
-    duration: "100%",
+    triggerElement: 'header',
+    duration: '200%',
     triggerHook: 0,
 })
     .setTween(timeline)
-    .setPin('.goal__section')
+    .setPin('section')
     .addTo(controller)
